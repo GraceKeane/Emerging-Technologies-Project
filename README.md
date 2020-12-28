700dda72fa5cbef6de3773696b383a7468be5142
GraceKeane <gracekeane82@gmail.com>
2020-12-28 12:01:58 +0000

    Set up project by adding notebook, bootstrap, index.html and python script

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Emerging-Technologies-Project
+# Emerging Technologies project
+
+#### Run web service
+
+'''bash
+$ export FLASK_APP=web-service.py
+$ python -m flask run
+ * Running on http://127.0.0.1:5000/
+ '''
---
+++ web-service.py
@@ -0,0 +1,10 @@
+# Referances
+# Adapted from - Flask; A Minimal Application;
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
