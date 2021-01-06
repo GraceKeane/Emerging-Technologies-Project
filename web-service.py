85fe847a70050d4a81f194991f0c7280ab00dc08
GraceKeane <gracekeane82@gmail.com>
2021-01-06 18:52:24 +0000

    Got simple web service up and running

--- web-service.py
+++ web-service.py
@@ -1,10 +1,15 @@
 # Referances
 # Adapted from - Flask; A Minimal Application;
 # https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application
+import flask as fl
+import numpy as np
 
-from flask import Flask
-app = Flask(__name__)
+app = fl.Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/uniform')
+def uniform():
+    return {'value': np.random.uniform()}
+
+@app.route('/normal')
+def normal():
+    return{"value": np.random.normal()}
