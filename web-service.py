744d4699bcdd682f81b0331125b30c936678fefb
GraceKeane <gracekeane82@gmail.com>
2021-01-07 20:16:27 +0000

    Got web-service working by fixing small errors and added comments

--- web-service.py
+++ web-service.py
@@ -1,25 +1,28 @@
-# flask for web app.
+# Imports needed
 import flask as fl
-# numpy for numerical work.
 import numpy as np
-
 import tensorflow as tf
+from flask import request
 
 # Create a new web app.
 app = fl.Flask(__name__)
 
-# Add root route.
+# Add root route. 
+# Connects to style page (index.html)
 @app.route("/")
 def home():
   return app.send_static_file('index.html')
 
-@app.route("/powerVal", method=["POST"])
-def powerValuesOutput():
-  ws = float(request.get_json()["val"])
-  projectModel = tf.keras.models.load_projectModel("savedValues.h5")
-  pred = projectModel.predict([windspeed])
-  listPred = prediction.tolist()
-  return {'prediction': listPred[0]}
+# Adapted from
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/#http-methods
+# 
+@app.route("/power", methods=["POST"])
+def powerOutput():
+  ws = float(request.get_json()["value"])
+  model = tf.keras.models.load_model("savedValues.h5")
+  prediction = model.predict([ws])
+  lp = prediction.tolist()
+  return {'prediction': lp[0]}
 
-if __name__ == 'main':
+if __name__ == '__main__':
   app.run(debug=True)
